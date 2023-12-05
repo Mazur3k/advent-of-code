6db3ef3894be21f411f4c134e858db8e458646d9
Adam Mazurkiewicz <adam.mazurkiewicz@cg24.com>
2023-12-05 23:55:55 +0100

    some adjustment to new requirements

--- src/main/scala/dayone/Calibrator.scala
+++ src/main/scala/dayone/Calibrator.scala
@@ -22,9 +22,26 @@ class FileReader {
 class Converter {
   def getNumbers(list: List[String]): List[Int] = {
     list
+      .map(input => replaceStringWithNumber(input))
       .map(input => input.filter(_.isDigit))
       .map(input => if(input.length == 1) input + input else input)
       .map(inputDigits => inputDigits.head.toString + inputDigits.last.toString)
       .map(_.toInt)
+  }
+  def replaceStringWithNumber(input: String): String = {
+    val map = Map(
+      "one" -> "1",
+      "two" -> "2",
+      "three" -> "3",
+      "four" -> "4",
+      "five" -> "5",
+      "six" -> "6",
+      "seven" -> "7",
+      "eight" -> "8",
+      "nine" -> "9",
+    )
+    var output = ""
+    map.foreach(entry => output = input.replace(entry._1, entry._2))
+    output
   }
 }
